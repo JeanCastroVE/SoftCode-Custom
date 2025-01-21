9f66c9a43bcf89d30636520714007be3e01dd1f2
Jean Castro <jeancastro.developerve@gmail.com>
2025-01-21 12:48:40 -0400

    [ADD]: Iniciando modulo de customización

---
+++ sale_custom/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    "name": "Sale Custom",
+    "summary": """
+        Customizaciones de Sale
+        """,
+    "author": "Softcode",
+    "contributors": [
+        "Jean Castro <jeancastro.developerve@gmail.com>"
+    ],
+    "license": "LGPL-3",
+    "website": "",
+    "category": "Softcode",
+    "version": "17.1",
+    "depends": [],
+    "data": [],
+    "assets": {},
+    "installable": True,
+    "application": True,
+    "auto_install": False,
+}
